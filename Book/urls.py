bab36570ebe42366b428456b3a822b0a694fc62b
Prashanth_Kumar_Devarakonda <prashanth.devarakonda@techgrit.com>
2023-02-27 14:13:47 +0530

    Logins completed

--- Book/urls.py
+++ Book/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 urlpatterns=[
     path("Signup", views.SignUp.as_view(), name="signup"),
+    path("Home", views.ProjectHome.as_view(), name="home"),
     path("logout/",views.logoutUser, name="logout"),
-    path("Login", views.Login.as_view(), name="login"),
-    path("Home", views.Home.as_view(), name="home"),
+
+    path("UserLogin", views.UserLogin.as_view(), name="userlogin"),
+    path("UserHome", views.UserHome.as_view(), name="userhome"),
+
+    path("LibraryLogin", views.LibraryLogin.as_view(), name="librarylogin"),
+    path("LibraryHome", views.LibraryHome.as_view(), name="libraryhome"),
+
+    path("BookstoreLogin", views.BookstoreLogin.as_view(), name="bookstorelogin"),
+    path("BookstoreHome", views.BookstoreHome.as_view(), name="bookstorehome"),
+
 ]
--- Book/views.py
+++ Book/views.py
@@ -6,7 +6,33 @@ from .models import Library, Bookstore, Customer, CustomerProfile, LibraryProfil
 from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+# Create your views here
+
+
+#@login_required(login_url='loginpage')
+class ProjectHome(View):
+    def post(self, request):
+        userbutton = request.POST['user']
+        librarybutton = request.POST['library']
+        bookstorebutton = request.POST['bookstore']
+
+
+        if userbutton=='User':
+            return redirect('userlogin')
+        
+        elif librarybutton=='Library':
+            return redirect('librarylogin')
+        
+        elif bookstorebutton=='Bookstore':
+            return redirect('bookstorelogin')
+
+    def get(self, request):
+        return render(request, "Book/homepage.html")
+
+def logoutUser(request):
+    logout(request)
+    #messages.info(request, 'Logout successful')
+    return redirect('login')
 
 class SignUp(View):
     def post(self, request):
@@ -36,38 +62,95 @@ class SignUp(View):
                 # )
                 # details.save()
 
-            return render(request, "Book/home.html")
+            return render(request, "Book/homepage.html")
 
 
     def get(self, request):
         return render(request, "Book/signup.html")
+    
+#--------------------------------------------------------
 
-class Login(View):
+class UserLogin(View):
     def post(self, request):
         username=request.POST['username']
         password=request.POST['pwd']
 
-        user= authenticate(request, username=username, password=password)
+        details = Customer.objects.get(role = 'Customer', username= username)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username Or Password is incorrect')
-            return render(request, "Book/login.html")
+        if details.role == 'CUSTOMER':
+            user= authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('userhome')
+            else:
+                messages.info(request, 'Username Or Password is incorrect')
+                return render(request, "Book/login.html")
         
     def get(self, request):
         return render(request, "Book/login.html")
-    
-def logoutUser(request):
-    logout(request)
-    #messages.info(request, 'Logout successful')
-    return redirect('login')
 
-#@login_required(login_url='loginpage')
-class Home(View):
+class UserHome(View):
     def post(self, request):
         pass
 
     def get(self, request):
-        return render(request, "Book/home.html")
+        return render(request, "Book/Userhome.html")
+    
+#-----------------------------------
+
+class BookstoreLogin(View):
+    def post(self, request):
+        username=request.POST['username']
+        password=request.POST['pwd']
+
+        details = Bookstore.objects.get(role = 'Bookstore', username= username)
+
+        if details.role == 'BOOKSTORE':
+            user= authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('bookstorehome')
+            else:
+                messages.info(request, 'Username Or Password is incorrect')
+                return render(request, "Book/Bookstorelogin.html")
+        
+    def get(self, request):
+        return render(request, "Book/Bookstorelogin.html")
+
+class BookstoreHome(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        return render(request, "Book/Bookstorehome.html")
+    
+#---------------------------------------------------------------
+
+class LibraryLogin(View):
+    def post(self, request):
+        username=request.POST['username']
+        password=request.POST['pwd']
+
+        details = Library.objects.get(role = 'Library', username= username)
+
+        if details.role == 'LIBRARY':
+            user= authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('libraryhome')
+            else:
+                messages.info(request, 'Username Or Password is incorrect')
+                return render(request, "Book/Librarylogin.html")
+        
+    def get(self, request):
+        return render(request, "Book/librarylogin.html")
+
+class LibraryHome(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        return render(request, "Book/Libraryhome.html")
